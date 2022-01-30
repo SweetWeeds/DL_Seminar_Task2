bf0cbc92298ccfb6e88ea41551c057d00f441873
SweetWeeds <khk4502@gmail.com>
2022-01-30 13:05:02 +0900

    220130_1305

--- task2/task2/custom_nn.hpp
+++ task2/task2/custom_nn.hpp
@@ -1,54 +1,126 @@
+#ifndef _CUSTOM_NN_H
+#define _CUSTOM_NN_H
+
 #include <cstdio>
 #include <string>
 
-#define DEBUG_SSL
-
 namespace custom_nn {
     namespace tensor {
+        int TENSOR_ID_CNT = 0;
+
+        /**
+         * Tensor
+         */
         template <typename T>
         class Tensor {
         private:
-            T* data;
-            size_t size;
-            int tensor_id;
+            T* data = nullptr;
+            size_t size = 0;
+            const int tensor_id;
         public:
-            static int tensor_id_cnt;
-            Tensor(size_t size) : size(size) {
-                this->data = new T[size];
-                tensor_id = tensor_id_cnt++;
+            // Constructor
+            Tensor(size_t size=0) : size(size), tensor_id(TENSOR_ID_CNT++) {
+                if (size > 0) {
+                    this->data = new T[size];
+                    memset(data, 0, size);
+                }
+                else {
+                    this->data = nullptr;
+                }
             }
+
+            // Copy Constructor
+            Tensor(const Tensor& t) {
+                delete[] this->data;
+                this->data = new T[t.getSize()];
+                for (int i = 0; i < t.getSize(); i++) {
+                    this->data[i] = t[i];
+                }
+            }
+
+            // Destructor
             ~Tensor() {
                 delete[] this->data;
             }
-            T operator[](int i){
+
+            // Indexing Operator Overloading
+            T& operator[](size_t i){
                 if (i > this->size || i < 0) {
                     fprintf(stderr, "[ERROR] Indexing out of range in %d tensor. (index: %d, size: %d)\n",
                         tensor_id, i, this->size);
                 }
                 return this->data[i];
             }
+
+            size_t getSize(void) {
+                #ifdef DEBUG_TENSOR
+                printf("[INFO] Tensor %d's size: %d\n", this->tensor_id, this->size);
+                #endif
+                return size;
+            }
+
+            int getID(void) {
+                return this->tensor_id;
+            }
+
+            void resize(size_t new_size) {
+                delete[] this->data;
+                this->data = new T[new_size];
+                this->size = new_size;
+            }
         };
     }
 
     namespace layer {
+        /**
+         * Layer
+         */
+        int LAYER_ID_CNT = 0;
         template <typename T>
         class Layer {
         private:
-            tensor::Tensor* p_weights;
-            tensor::Tensor* p_bias;
+            tensor::Tensor<T> weights;      // Weight
+            tensor::Tensor<T> bias;         // Bias
+            tensor::Tensor<T> d_weights;    // Weight Derivative
+            tensor::Tensor<T> d_bias;       // Bias Derivative
+            int layer_id;
         public:
-            Layer((size_t w_size, size_t b_size)) {
-                p_weights = new tensor::Tensor<T>(w_size);
-                p_bias    = new tensor::Tensor<T>(b_size);
+            Layer(size_t w_size=0, size_t b_size=0) : layer_id(LAYER_ID_CNT++) {
+                weights.resize(w_size);
+                d_weights.resize(w_size);
+                bias.resize(b_size);
+                d_bias.resize(b_size);
             }
-            tensor::Tensor forward(tensor::Tensor p_x) {
-                #ifdef DEBUG_SSL
+            Layer(tensor::Tensor<T> weights, tensor::Tensor<T> bias) {
+                this->weights = weights;
+                this->bias    = bias;
+            }
+            // Forward
+            tensor::Tensor<T> forward(tensor::Tensor<T>* p_x) {
+                #ifdef DEBUG_LAYER
+                printf("[INFO:Layer %02d] Forward\n", this->layer_id);
                 #endif
             }
-            tensor::Tensor backward(tensor::Tensor p_dout) {
+            /**
+             * Back Propagation
+             * Return: dout
+             */
+            tensor::Tensor<T> backward(tensor::Tensor<T>* p_dout) {
+                #ifdef DEBUG_LAYER
+                printf("[INFO:Layer %02d] Backward\n", this->layer_id);
+                #endif
+            }
+            // Weight Update
+            void update(T lr) {
+                #ifdef DEBUG_LAYER
+                printf("[INFO:Layer %02d] Update\n", this->layer_id);
+                #endif
+                for (int i = 0; i < weights.getSize(); i++)
+                    weights[i] += lr * d_weights[i];
+                for (int i = 0; i < bias.getSize(); i++)
+                    bias[i]    += lr * d_bias[i];
 
             }
-
         };
 
         template <typename T>
@@ -68,13 +140,30 @@ namespace custom_nn {
 
         template <typename T>
         class ReLU : public Layer<T> {
+        private:
 
+        public:
+            tensor::Tensor<T> forward(tensor::Tensor<T> x) {
+                size_t x_size = x.getSize();
+                tensor::Tensor<T> ret_T(x_size);
+                for (size_t i = 0; i < x_size; i++) {
+                    ret_T[i] = x[i] < 0 ? 0 : x[i];
+                }
+                return ret_T;
+            }
+
+            tensor::Tensor<T> backward(tensor::Tensor<T> d_out) {
+
+            }
         };
 
         template <typename T>
         class MaxPool2D : public Layer<T> {
+        private:
+        public:
 
         };
     }
 }
 
+#endif

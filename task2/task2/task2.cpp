dc43cab16a1f7864f5c434b5aa692792ef78915c
SweetWeeds <khk4502@gmail.com>
2022-02-18 20:45:10 +0900

    220218_2040

--- task2/task2/task2.cpp
+++ task2/task2/task2.cpp
@@ -1,12 +1,46 @@
-﻿#include <iostream>
-#include <cstdio>
-#include "custom_nn.hpp"
+﻿/**
+ *
+ * Deep Learning Seminar Task 2: Custom Neural Netweork Build with C++
+ *
+ * Written by Kwon, Han Kyul
+ * File name: task2.cpp
+ * Program date: 2022.02.17.
+ *
+ * Written on Feb 17, 2022
+ * Modified on Feb 17, 2022
+ * Modification History:
+ *      1. Written by Hankyul Kwon
+ *      2. Modified by Han Kyul Kwon on April 27, 2017
+ *          (a) Add codes for normal execution. (on parctice hours)
+ *
+ * Compiler used: MSVC++ 14.16 (Visual Studio 2017 version 15.9)
+ *
+ */
 
-using namespace custom_nn::tensor;
+#include <iostream>
+#include <cstdio>
+#include "tensor.hpp"
+#include "vgg8.hpp"
+#include "data_loader.hpp"
+
+using namespace tensor;
+using namespace layer;
+using namespace model;
+
 
 int main() {
-    int t1_size[2] = { 1, 2 };
-    Tensor<float> t1(2, t1_size);
-    printf("t1 size:%d, %f, %f\n", t1.getSize(), t1[0], t1[1]);
+    const int shape[3] = { 1, 2, 3 };
+    Tensor<float> t1(3, shape);
+    t1[0] = 1.0;
 
+    /*
+    Model<float>* VGG8 = build_vgg8<float>();
+    VGG8->load("vgg8_params.bin");
+    printf("[INFO] Forward\n");
+    VGG8->forward(&t1);
+    printf("[INFO] Backward\n");
+    VGG8->backward(&t1);
+    */
+    float *data;
+    int data_size = load_data(data, "test.bin");
 }
